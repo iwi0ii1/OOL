45400ad8a5415fd1195c5e2f73abb4c978bffa99
iwi0ii1 <tangyenrui@gmail.com>
2026-02-26 19:45:45 +0800

    Added insert (2 overloads), erase, push_back, pop_back

--- base/contiguous_storage.hpp
+++ base/contiguous_storage.hpp
@@ -60,11 +60,11 @@ namespace asl::base {
             return data_[used_slots_ - 1];
         }
 
-        inline bool starts_with(T v) const noexcept {
+        inline bool starts_with(const T& v) const noexcept {
             return !empty() && front() == v;
         }
 
-        inline bool ends_with(T v) const noexcept {
+        inline bool ends_with(const T& v) const noexcept {
             return !empty() && back() == v;
         }
 
@@ -86,38 +86,44 @@ namespace asl::base {
 
 
 
-        iterator begin() noexcept {
+        inline iterator begin() noexcept {
             return data_;
         }
 
-        iterator end() noexcept {
+        inline iterator end() noexcept {
             return data_ + used_slots_;
         }
 
-        const_iterator cbegin() const noexcept {
+        inline const_iterator cbegin() const noexcept {
             return data_;
         }
 
-        const_iterator cend() const noexcept {
+        inline const_iterator cend() const noexcept {
             return data_ + used_slots_;
         }
 
-        reversed_iterator rbegin() noexcept {
+        inline reversed_iterator rbegin() noexcept {
             return reversed_iterator(data_);
         }
 
-        reverse_iterator rend() noexcept {
+        inline reverse_iterator rend() noexcept {
             return reversed_iterator(data_ + used_slots_);
         }
 
-        const_reversed_iterator crbegin() const noexcept {
+        inline const_reversed_iterator crbegin() const noexcept {
             return const_reversed_iterator(data_);
         }
 
-        const_reversed_iterator crend() const noexcept {
+        inline const_reversed_iterator crend() const noexcept {
             return const_reversed_iterator(data_ + used_slots_);
         }
         #pragma endregion
+
+
+
+
+
+
 
 
         #pragma region Mutators
@@ -137,32 +143,63 @@ namespace asl::base {
 
 
 
+        // Insert an element
+        // @param pos Where to insert
+        // @param val What to insert
+        // @note Will allocate only 1 more slot if insufficient (no extra growth)
+        inline iterator insert(iterator pos, const T& val) {
+            const size_t offset = pos - this->begin();
 
-        inline iterator insert(const size_t pos, const T& val) {
-            if (used_slots_ >= slots_ || slots_ == 0)
+            if (used_slots_ >= slots_ || slots_ == 0) {
                 reserve(1);
+                pos = this->begin() + offset;
+            }
 
-            std::move_backward(data_ + start, data_ + used_slots_, data_ + used_slots_ + 1);
-
-            data_[start] = value;
+            // std::move_backward() dereferences, so we construct decoys
+            new (this->end()) T;
+            auto new_pos = std::move_backward(pos, this->end(), this->end() + 1);
+            new (pos) T{val};
+            
             used_slots_ += 1;
-            return &data_[start];
+            return new_pos;
         }
 
-        inline iterator push_back
-
-        inline void erase(const size_t index, const bool left_shift = true) {
-            if (index >= used_slots_)
-                    throw std::out_of_range("Index out of range...");
-
-            if (left_shift)
-                std::move(data_ + index + 1, data_ + used_slots_, data_ + index);
-
-            used_slots_ -= 1;
-            if constexpr (!std::is_trivially_destructible_v<_data_type>)
-                data_[used_slots_].~_data_type();
+        // Insert an element to the back
+        inline iterator push_back(const T& val) {
+            return insert(this->end(), val);
         }
 
+        // Insert a range of elements
+        // @param pos Where to insert
+        // @param first Start range of elements
+        // @param last End range of elements
+        // @note Will allocate more memory if insufficient (no extra growth)
+        iterator insert(iterator pos, iterator first, iterator last);
+
+        // Remove elements
+        // @param first The first element to remove
+        // @param last Until the last element to remove
+        // @note Doesn't reduce slots
+        inline void erase(iterator first, iterator last = first + 1) {
+            if (first == last)
+                throw std::invalid_argument("asl::base::contiguous_storage<T>::erase(...): `first` iterator the same as `last` iterator.")
+
+            // Bring the "chosen ones" to overwrite starting from `first` (third parameter)
+            // Returns the start of the ghosts (old objects whom dtors aren't called yet)
+            auto ghosts_pos = std::move(last, this->end(), first);
+            std::destroy(ghosts_pos, this->end()); // Runs dtor of ghosts
+
+            used_slots_ -= last - first;
+        }
+
+        // Remove the last element
+        // @param rep Removal repetition
+        inline void pop_back(const size_t rep = 1) {
+            erase(this->end() - rep, this->end())
+        }
+
+        // Clear elements
+        // @note Doesn't reduce slots
         inline void clear() {
             std::destroy_n(data_, used_slots_);
             used_slots_ = 0;
@@ -194,11 +231,36 @@ namespace asl::base {
         else if constexpr (std::is_copy_constructible_v<T>)
             std::uninitialized_copy_n(data_, elements_to_transfer, new_memory);            
 
-        std::destroy_n(data_, elements_to_transfer);
+        if constexpr (std::is_trivial_v<T>)
+            std::destroy_n(data_, elements_to_transfer);
+        
         ::operator delete(data_);
 
         data_ = new_memory;
         slots_ = slots_number;
         used_slots_ = elements_to_transfer;
+    }
+
+
+    template<typename T>
+    requires storage_compatible<T>
+    constiguous_storage<T>::iterator constiguous_storage<T>::insert(iterator pos, iterator first, iterator last) {
+        const size_t offset = pos - this->begin();
+        const size_t range_diff = last - first;
+
+        if (used_slots_ + range_diff > slots_ || slots_ == 0) {
+            reserve(range_diff);
+            pos = this->begin() + offset;
+        }
+
+        for (size_t i = 0; i < range_diff; i++)
+            new (this->end() + i) T;
+
+        auto new_pos = std::move_backward(pos, this->end(), this->end() + range_diff);
+
+        std::uninitialized_copy(first, last, pos);
+
+        used_slots_ += range_diff;
+        return new_pos;
     }
 }
