387d075c1119d15dd8e44d2c4431a48fcfa30099
iwi0ii1 <tangyenrui@gmail.com>
2026-03-02 14:15:27 +0800

    bruh

--- base/contiguous_storage.hpp
+++ base/contiguous_storage.hpp
@@ -155,25 +155,12 @@ namespace asl::base {
 
 
 
-        // Insert an element
+        // Insert a range of elements
         // @param pos Where to insert
-        // @param val What to insert
-        // @note Will allocate only 1 more slot if insufficient (no extra growth)
-        inline iterator insert(const_iterator pos, const T& val) {
-            if (used_slots_ >= slots_ || slots_ == 0)
-                reserve(1);
-
-            iterator* insert_pos = begin() + (pos - begin());
-            iterator* end_pos = end() - 1
-            
-            new (end()) T(end_pos); // Construct last element at end()
-            for (; end_pos != insert_pos; --end_pos)
-                *end_pos = *(end_pos - 1);
-
-            
-            used_slots_ += 1;
-            return insert_pos;
-        }
+        // @param first Start range of elements
+        // @param last End range of elements
+        // @note Will allocate more memory if insufficient (no extra growth)
+        iterator insert(iterator pos, const_iterator first, const_iterator last);
 
         // Insert an element to the back
         // @param val Value to add
@@ -192,13 +179,6 @@ namespace asl::base {
             return first_iterator;
         }
 
-        // Insert a range of elements
-        // @param pos Where to insert
-        // @param first Start range of elements
-        // @param last End range of elements
-        // @note Will allocate more memory if insufficient (no extra growth)
-        iterator insert(iterator pos, const_iterator first, const_iterator last);
-
         // Remove elements
         // @param first The first element to remove
         // @param last Until the last element to remove
@@ -207,15 +187,11 @@ namespace asl::base {
             if (first == last)
                 throw std::invalid_argument("asl::base::contiguous_storage<T>::erase(...): `first` iterator the same as `last` iterator.");
             
-            const size_t idx_first = first - begin();
-            const size_t idx_last = last - end();
+            iterator mut_first = begin() + (first - begin());
+            iterator mut_last = begin() + (last - begin());
 
-            // Bring the "chosen ones" to overwrite starting from `first` (third parameter)
-            // Returns the start of the ghosts (old objects whom dtors aren't called yet)
-            auto ghosts_pos = std::move(begin() + idx_last, end(), begin() + idx_first);
-            std::destroy(ghosts_pos, end()); // Runs dtor of ghosts
-
-            used_slots_ -= last - first;
+            std::destroy(std::move(mut_last, end(), mut_first), end());
+            used_slots_ -= (last - first);
         }
 
         // Remove the last element
@@ -271,26 +247,19 @@ namespace asl::base {
     template<a_regular_value T>
     requires storage_compatible<T>
     contiguous_storage<T>::iterator contiguous_storage<T>::insert(iterator pos, const_iterator first, const_iterator last) {
-        const size_t idx_first = first - begin();
-        const size_t idx_last = last - begin();
+        iterator mut_first = begin() + (first - begin());
+        iterator mut_last = begin() + (last - begin());
+        const size_t count = last - first;
 
-        const size_t offset = pos - begin();
-        const size_t range_diff = idx_last - idx_first;
-
-        if (used_slots_ + range_diff > slots_ || slots_ == 0) {
-            reserve(range_diff);
-            pos = begin() + offset;
+        if (used_slots_ + count > slots_) {
+            reserve(count);
+            pos = data_ + (pos - data_);
         }
 
-        for (size_t i = 0; i < range_diff; i++)
-            new (end() + i) T;
-
-        std::move_backward(pos, end(), end() + range_diff);
-
-        // first / last are const -> both const / non-const will work
-        std::uninitialized_copy(begin() + idx_first, begin() + idx_last, pos);
-
-        used_slots_ += range_diff;
+        std::move_backward(pos, end(), end() + count); // Move everything rightward
+        std::uninitialized_copy(first, last, pos);
+        
+        used_slots_ += count;
         return pos;
     }
 }
